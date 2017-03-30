29b8150ee9c09244a0485848e02e009099158dd3
Clemens BlanK <blankclemens@gmail.com>
2017-03-30 11:54:59 +0000

    Small fixes to make the very basics work - e.g. as a tech demo.

--- content-script.js
+++ content-script.js
@@ -11,10 +11,10 @@ function toggleTourRecording(toggle){
 function notifyExtension(e) {
     if (recording){
         if ('tour-toggle' === event.target.id) {
-            var h = $('#tour-configurator').css('height');
-            $('#tour-configurator').css('height', '20px' === h ? '200px' : '20px');
+            var h = $('#tour-config').css('height');
+            $('#tour-config').css('height', '20px' === h ? '200px' : '20px');
             return;
-        }
+        };
         var path = $(event.target).domPath().join('');
         if (!/tour-configurator/.test(path)) {
             addStep(path);
@@ -26,16 +26,17 @@ function addStep(path) {
     var $text = $('#tour-textarea');
     var content = $text.val();
     if ('' !== path) {
-        content = content + '\n  - title: \'Step ' + nbSteps++ + '\'\n    element: \'' + path + '\'\n    intro: \'\'\n        position: \'\'\n';
+        content = content + '\n  - title: \'Step ' + nbSteps++ + '\'\n    element: \'' + path + '\'\n    content: \'\'\n    placement: \'\'\n    postclick: \n      - ' + path + '\n';
     }
     $text.val(content);
-}
-
-$('body').append('\n    <div style="position: absolute; bottom: 0; height: 200px; width: 100%; z-index: 1000;" id="tour-config>\n      <button id="tour-toggle">toggle</button>\n      <textarea style="width: 100%; height: 100%;" id="tour-textarea">' + defaultContent + '</textarea>\n    </div>');
+};
 
 window.addEventListener("click", notifyExtension);
 chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
   if (msg.action == 'toggle_tour_record') {
+      if (msg.value === true && !$('#tour-config').length) {
+        $('body').append('\n    <div style="position: absolute; bottom: 0; height: 200px; width: 100%; z-index: 1000;" id="tour-config">\n      <button id="tour-toggle">toggle</button>\n      <textarea style="width: 100%; height: 100%;" id="tour-textarea">' + defaultContent + '</textarea>\n    </div>');
+      };
       toggleTourRecording(msg.value);
   }
 });
